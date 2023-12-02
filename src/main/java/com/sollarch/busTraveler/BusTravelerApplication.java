598f599bf34799f01933199f86779812aadf7746
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-03 00:09:58 +0300

    Initial commit

---
+++ src/main/java/com/sollarch/busTraveler/BusTravelerApplication.java
@@ -0,0 +1,13 @@
+package com.sollarch.busTraveler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTravelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTravelerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sollarch/busTraveler/BusTravelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sollarch.busTraveler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTravelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
